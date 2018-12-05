d1e465489b22e01cea505336d76b9607c9212d9d
novice43 <bigwilly4307@gmail.com>
2018-12-05 19:39:23 +0400

    Server with JNDI. FOCKIN SHIT

--- src/servletExample.java
+++ src/servletExample.java
@@ -1,17 +1,47 @@
+import javax.annotation.Resource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.Statement;
+
 
 @WebServlet("/test")
 public class servletExample extends HttpServlet
 {
+    @Resource(name = "jdbc/db")
+    private DataSource ds;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        System.out.println("test");
-        super.doGet(req, resp);
+        PrintWriter pw = resp.getWriter();
+        try
+        {
+            Context ctx = new InitialContext();
+            Connection c = ds.getConnection();
+            Statement s = c.createStatement();
+            pw.println("execute = " + s.execute("INSERT INTO ss(test) VALUES ('fix');"));
+            s.close();
+            c.close();
+        }
+        catch(Exception ex)
+        {
+            pw.println("FUCK, EXCEPTION");
+            pw.println(ex.toString());
+            pw.flush();
+        }
+        finally
+        {
+            pw.close();
+        }
     }
 }
